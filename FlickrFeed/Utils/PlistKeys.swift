8297ea6e6662c52ca005f9c5c67b99f753b083a7
Amber Katyal <amberkatyal@icloud.com>
2021-01-14 22:44:06 +0530

    Added PlistKeys Util

---
+++ FlickrFeed/Utils/PlistKeys.swift
@@ -0,0 +1,24 @@
+//
+//  PlistKeys.swift
+//  FlickrFeed
+//
+//  Created by Amber Katyal on 14/01/21.
+//  Copyright © 2021 Amber Katyal. All rights reserved.
+//
+
+import Foundation
+
+enum PlistKeys: String {
+    case FLICKR_API_KEY
+    case FLICKR_API_SECRET
+    
+}
+
+/// To support get values in Info.plist with key
+///
+/// - Parameter key: key of value that we want to get
+/// - Returns: value of key that we gived
+func infoForKey(_ key: PlistKeys) -> String {
+    return (Bundle.main.infoDictionary?[key.rawValue] as? String)?
+        .replacingOccurrences(of: "\\", with: "") ?? ""
+}
