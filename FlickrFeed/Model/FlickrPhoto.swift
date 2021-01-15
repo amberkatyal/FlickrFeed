eceff3584c1f56df1e18086c6c504058301866b5
Amber Katyal <amberkatyal@icloud.com>
2021-01-15 12:00:09 +0530

    Adding FlickfPhoto And FlickrPhotoSize model

---
+++ FlickrFeed/Model/FlickrPhoto.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrPhoto.swift
+//  FlickrFeed
+//
+//  Created by Amber Katyal on 15/01/21.
+//  Copyright © 2021 Amber Katyal. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrPhoto {
+    let id: UUID
+    let title: String?
+    let secret: UUID
+    let serverID: UUID
+}
---
+++ FlickrFeed/Model/FlickrPhotoSize.swift
@@ -0,0 +1,23 @@
+//
+//  FlickrPhotoSize.swift
+//  FlickrFeed
+//
+//  Created by Amber Katyal on 15/01/21.
+//  Copyright © 2021 Amber Katyal. All rights reserved.
+//
+
+import Foundation
+
+enum FlickrPhotoSize: String {
+    case thumbnailShort     = "s"
+    case thumbnailCropped   = "q"
+    case thumbnail          = "t"
+    case smallMedium        = "m"
+    case smallNormal        = "n"
+    case smallWide          = "w"
+    case medium             = "z"
+    case medium800          = "c"
+    case large1024          = "b"
+    case large1600          = "h"
+    case orignal            = "o"
+}
