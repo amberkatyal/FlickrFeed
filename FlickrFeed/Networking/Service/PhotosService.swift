5fc4a368bd6de74d453f55e7fa28188eb3cda48e
Amber Katyal <amberkatyal@icloud.com>
2021-01-25 12:53:39 +0530

    Creating PhotoFeedMapper tot parse and separate API implementation details from the main module.

--- FlickrFeed/Networking/Service/PhotosService.swift
+++ FlickrFeed/Networking/Service/PhotosService.swift
@@ -12,12 +12,58 @@ struct PhotosService: WebService {
     
     let router = Router<PhotosAPI>()
     
-    func fetchPopularPhotos(batchSize: Int = 15, page: Int = 2) {
+    func fetchPopularPhotos(batchSize: Int, page: Int, completion: @escaping (Result<FlickrPage,NetworkResponse>)->Void) {
         router.request(.getPopular(batchSize: batchSize, page: page)) { (data, response, error) in
-            self.parseAPIResponse(data: data, response: response, error: error) { (_) in
-                
+            self.parseAPIResponse(data: data, response: response, error: error) { (result) in
+                let result = PhotoFeedMapper.map(data!, result: result)
+                completion(result)
             }
         }
     }
     
+}
+
+internal final class PhotoFeedMapper {
+    private struct Response: Decodable {
+        let page: RemotePage
+        
+        enum CodingKeys: String, CodingKey {
+            case page = "photos"
+        }
+    }
+    
+    private struct RemotePage: Decodable {
+        public let page, pages, perpage, total: Int
+        public let photo: [RemotePhoto]
+        
+        var flickrPage: FlickrPage {
+            return FlickrPage(page: page, pages: pages, perpage: perpage, total: total, photo: photo.map { $0.photo })
+        }
+    }
+    
+    private struct RemotePhoto: Decodable {
+        public let id: String
+        public let title: String
+        public let secret: String
+        public let server: String
+        
+        var photo: FlickrPhoto {
+            return FlickrPhoto(id: id, title: title, secret: secret, server: server)
+        }
+    }
+
+    internal static func map(_ data: Data, result: Result<Any, NetworkResponse>) -> Result<FlickrPage, NetworkResponse>{
+        switch result {
+        case .success:
+            do {
+                let response = try JSONDecoder().decode(Response.self, from: data)
+                return .success(response.page.flickrPage)
+            } catch {
+                return .failure(.unableToDecode)
+            }
+        case .failure(let error):
+            debugPrint(error)
+            return .failure(error)
+        }
+    }
 }
