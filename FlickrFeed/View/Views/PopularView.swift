d573f5724b7300aebe23584abd618dad1eb46b5f
Amber Katyal <amberkatyal@icloud.com>
2021-01-25 13:44:07 +0530

    Adding segment control

--- FlickrFeed/View/Views/PopularView.swift
+++ FlickrFeed/View/Views/PopularView.swift
@@ -11,6 +11,13 @@ import UIKit
 final class PopularView: UIView {
     
     // MARK: - Views
+    let segmentControl: UISegmentedControl = {
+        let segment = UISegmentedControl(items: ["2", "3", "4"])
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        segment.selectedSegmentIndex = 0
+        return segment
+    }()
+    
     let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.sectionInset = .zero
@@ -34,8 +41,16 @@ final class PopularView: UIView {
     // MARK: - Setup
     private func setup() {
         backgroundColor = .systemBackground
+        addSubview(segmentControl)
+        let safeArea = safeAreaLayoutGuide
+        segmentControl.centerXAnchor.constrainEqual(anchor: centerXAnchor)
+        segmentControl.topAnchor.constrainEqual(anchor: safeArea.topAnchor)
+        segmentControl.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.5, constant: 0).isActive = true
         addSubview(collectionView)
-        collectionView.constrainEdges(toLayoutGuide: self.safeAreaLayoutGuide)
+        collectionView.leadingAnchor.constrainEqual(anchor: safeArea.leadingAnchor)
+        collectionView.trailingAnchor.constrainEqual(anchor: safeArea.trailingAnchor)
+        collectionView.bottomAnchor.constrainEqual(anchor: safeArea.bottomAnchor)
+        collectionView.topAnchor.constrainEqual(anchor: segmentControl.bottomAnchor, constant: 8)
         collectionView.registerClass(PopularCollectionViewCell.self)
     }
 }
